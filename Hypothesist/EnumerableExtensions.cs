b095dd37ba40eaf7cfe522349f0e7e696cb27214
Manuel Riezebosch <mriezebosch@gmail.com>
2021-06-08 09:24:31 +0200

    fixup! refactor(api): removed builder and use IObserver and IObservable

--- Hypothesist/EnumerableExtensions.cs
+++ Hypothesist/EnumerableExtensions.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Hypothesist.Time
+namespace Hypothesist
 {
     internal static class EnumerableExtensions
     {
