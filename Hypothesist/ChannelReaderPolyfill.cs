81c4537b2cb10c6d4050b38b9b0508e79d373462
Manuel Riezebosch <mriezebosch@gmail.com>
2023-03-02 10:05:00 +0100

    refactor(channelreader): renamed polyfill and include source reference

---
+++ Hypothesist/ChannelReaderPolyfill.cs
@@ -0,0 +1,25 @@
+﻿#if !NETSTANDARD2_1_OR_GREATER
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Channels;
+
+namespace Hypothesist;
+
+/// <summary>
+/// source: https://github.com/dotnet/runtime/blob/95147163dac477da5177f5c5402ae9b93feb5c89/src/libraries/System.Threading.Channels/src/System/Threading/Channels/ChannelReader.netstandard21.cs
+/// </summary>
+internal static class ChannelReaderPolyfill
+{
+    public static async IAsyncEnumerable<T> ReadAllAsync<T>(this ChannelReader<T> reader, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        while (await reader.WaitToReadAsync(cancellationToken).ConfigureAwait(false))
+        {
+            while (reader.TryRead(out T item))
+            {
+                yield return item;
+            }
+        }
+    }
+}
+#endif
