3c0ea7e44ea2274793106699fc7d50b4c3cd668e
Manuel Riezebosch <mriezebosch@gmail.com>
2023-11-02 12:30:41 +0100

    refactor(aspnet): rename to TestFromRequest and add TestFromRequestBody

--- AspNet/Hypothesist.AspNet.Tests/Middleware.cs
+++ AspNet/Hypothesist.AspNet.Tests/Middleware.cs
@@ -1,17 +1,20 @@
 using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
 using FluentAssertions;
 using FluentAssertions.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Hypothesist.AspNet.Tests;
 
 public class Middleware
 {
-    private readonly Uri _url = new Uri("http://localhost:1234");
+    private readonly Uri _url = new("http://localhost:1234");
 
     [Fact]
-    public async Task Request()
+    public async Task FromQuery()
     {
         var input = Guid.NewGuid();
         var hypothesis = Hypothesis
@@ -19,7 +22,7 @@ public class Middleware
             .Any(s => s == input);
 
         await using var app = Setup(_url);
-        app.Use(hypothesis.AsMiddleware(request => Guid.Parse(request.Query["data"]!)));
+        app.Use(hypothesis.TestFromRequest(request => Guid.Parse(request.Query["data"]!)));
         
         await app.StartAsync();
         await Test(_url, input);
@@ -28,15 +31,33 @@ public class Middleware
     }
     
     [Fact]
-    public async Task Async()
+    public async Task UseWhen()
     {
         var input = Guid.NewGuid();
         var hypothesis = Hypothesis
-            .For<string>()
-            .Any(string.IsNullOrEmpty);
+            .For<Guid>()
+            .Any(s => s == input);
 
         await using var app = Setup(_url);
-        app.Use(hypothesis.AsMiddleware(async request => await new StreamReader(request.Body).ReadToEndAsync()));
+        app.UseWhen(context => context.Request.Path == "/hello",
+            then => then.Use(hypothesis.TestFromRequest(request => Guid.Parse(request.Query["data"]!))));
+        
+        await app.StartAsync();
+        await Test(_url, input);
+
+        await hypothesis.Validate(3.Seconds());
+    }
+    
+    [Fact]
+    public async Task FromBody()
+    {
+        var input = Guid.NewGuid();
+        var hypothesis = Hypothesis
+            .For<Guid>()
+            .Any(s => s == input);
+
+        await using var app = Setup(_url);
+        app.Use(hypothesis.TestFromRequestBody(body => JsonSerializer.DeserializeAsync<Guid>(body).AsTask()));
         
         await app.StartAsync();
         await Test(_url, input);
@@ -48,19 +69,23 @@ public class Middleware
     {
         var builder = WebApplication.CreateBuilder(new[] { $"--urls={url}" });
         var app = builder.Build();
-        app.MapGet("/hello", () => Results.Ok());
         app.UseDeveloperExceptionPage();
         
+        app.MapPost("/hello", ([FromBody]Guid body) => Results.Ok(body));
+
         return app;
     }
 
     private static async Task Test(Uri url, Guid input)
     {
         using var client = new HttpClient { BaseAddress = url };
-        using var response = await client.GetAsync($"hello?data={input}");
+        using var response = await client.PostAsJsonAsync($"hello?data={input}", input);
         response
             .StatusCode
             .Should()
             .Be(HttpStatusCode.OK, await response.Content.ReadAsStringAsync());
+
+        var result = await response.Content.ReadFromJsonAsync<Guid>();
+        result.Should().Be(input);
     }
 }
