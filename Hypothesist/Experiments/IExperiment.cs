d62040b43bdb3acf98a7ff2b108d5360e1b1dc3d
Manuel Riezebosch <mriezebosch@gmail.com>
2023-11-10 10:15:54 +0100

    refactor(experiments): all implement OnError instead from interface

--- Hypothesist/Experiments/IExperiment.cs
+++ Hypothesist/Experiments/IExperiment.cs
@@ -4,10 +4,5 @@ namespace Hypothesist.Experiments;
 
 public interface IExperiment<in T> : IObserver<T>
 {
-    #if NETSTANDARD2_1_OR_GREATER
-    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
-    void IObserver<T>.OnError(Exception error) => throw new NotImplementedException();
-    #endif
-
     bool Done { get; }
 }
